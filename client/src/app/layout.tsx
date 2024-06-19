b011b9b7eee92ce3d6597ca69e756545f3a210fb
Tquandoo <quantrungdo2401@gmail.com>
2024-06-19 12:18:38 +0700

    create and setup shadcn UI

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google"
+import { cn } from "@/lib/utils"
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+ 
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={fontSans.className}>{children}</body>
     </html>
   );
 }
